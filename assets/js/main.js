e9a49403d5f36798b687e60d9e375800d1b78349
Aldrin Renz Cruz <74097341+aldrinrenzcruz@users.noreply.github.com>
2024-11-01 07:02:01 +0800

    added functions to toggle the cart container

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+let shoppingCartBtn = document.querySelector("#shopping-cart-btn");
+
+shoppingCartBtn.addEventListener("click", () => { console.log("test") })
+
+function getElement(param) {
+  return typeof param === "string" ? document.querySelector(`#${param}`) : param;
+}
+
+function show(param, display_style) {
+  const element = getElement(param);
+  const display = display_style || "block";
+  if (element) {
+    element.style.opacity = 0;
+    element.style.display = display;
+    requestAnimationFrame(function () {
+      element.style.transition = "opacity 200ms";
+      element.style.opacity = 1;
+    });
+  } else {
+    console.warn("Element being shown not found or invalid parameter.");
+  }
+}
+
+function hide(param) {
+  const element = getElement(param);
+  if (element) {
+    element.style.opacity = 0;
+    requestAnimationFrame(function () {
+      element.style.transition = "opacity 200ms";
+      element.style.display = "none";
+    });
+  } else {
+    console.warn("Element being hidden not found or invalid parameter.");
+  }
+}
+
+function toggle(param, display_style) {
+  const element = getElement(param);
+  const display = display_style || "block";
+  if (element) {
+    if (window.getComputedStyle(element).display === "none") {
+      show(param, display);
+    } else {
+      hide(param);
+    }
+  } else {
+    console.warn("Element being toggled not found or invalid parameter.");
+  }
+}
